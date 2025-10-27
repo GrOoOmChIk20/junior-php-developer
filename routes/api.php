ac8f0640e0e9b823d79eacd794574ee56e342607
Илья Романенко <i.romanenko@yarus.pro>
2025-10-27 16:54:40 +0700

    Добавление маршрута

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\laravel_crud\ListTaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix('laravel-crud')->name('laravel-crud.')->group(function () {
+    Route::apiResource('list-task', ListTaskController::class);
 });
